a0c47ee13f09f49316315613ca9833045b2c4fa2
kt-std <kt.std2020@gmail.com>
2020-12-28 00:00:28 +0200

    Add response status check method

--- js/script.js
+++ js/script.js
@@ -1,6 +1,35 @@
-const URL = 'https://randomuser.me/api/?results=100';
+const USERS_AMOUNT = 5000,
+	API_URL = `https://randomuser.me/api/?results=${USERS_AMOUNT}`;
 
-fetch(URL)
-	.then(response => response.json())
-	.then(responseBody => console.log(responseBody))
-	.catch(error => console.log(error));
+fetch(API_URL)
+	.then((response) => {
+		if (checkResponseStatus(response.status)) {
+			return response.json();
+		} else {
+			console.log(response);
+			appendErrorMessage(getResponseErrorMessage(response.status, response.statusText));
+			return response.json();
+		}
+	})
+	.then((responseBody) => console.log(responseBody))
+	.catch((error) => console.log(typeof error));
+
+function checkResponseStatus(status) {
+	if (status >= 200 && status < 300) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function getResponseErrorMessage(status, statusText) {
+	return `<h1 class='error_heading'>Sorry, an error occured!</h1>
+			<h2 class='erro-code__heading'>${status}: ${statusText}</h2>`;
+}
+
+function appendErrorMessage(errorText){
+	const div = document.createElement('div');
+	div.innerHTML = errorText;
+	div.classList.add('error__block');
+	document.body.append(div);
+}
